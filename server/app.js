460dc19ee73c8b159ad40ee928db9c48fe2d34b6
Rishi Raj <116630999+rishi-o2@users.noreply.github.com>
2024-06-14 02:08:01 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,39 +1,45 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const PORT = 5000;
-require("./db/conn")
-require("./models/user")
-require("./models/Post")
+
+require("./db/conn");
+require("./models/user");
+require("./models/Post");
+
+// Configure CORS to allow requests from specific origin
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
 app.use(express.json());
-app.use(require("./routers/auth"))
-app.use(require("./routers/signin"))
-app.use(require("./routers/Post"))
-app.use(require("./routers/user"))
 
+app.use(require("./routers/auth"));
+app.use(require("./routers/signin"));
+app.use(require("./routers/Post"));
+app.use(require("./routers/user"));
 
-var cors = require("cors");
+const middleware = (req, res, next) => {
+    console.log("Middleware active");
+    next();
+};
 
-app.use(cors());
-const middleware= (req,res,next)=>{
-    console.log("Middleware active")
-    next()
-}
+app.get('/', (req, res) => {
+    res.send("Hello World");
+    console.log("Home");
+});
 
+app.get('/about', middleware, (req, res) => {
+    res.send("Hello About");
+    console.log("Hello");
+});
 
+app.get('/message', (req, res) => {
+    res.send("Successfull");
+});
 
-app.get('/',(req,res)=>{
-    res.send("hello World")
-    console.log("Home")
-})
-
-app.get('/about',middleware, (req,res)=>{
-    res.send("Hello About")
-    console.log("Hello")
-})
-app.get('/message',(req,res)=>{
-    res.send("Successfull")
-})
-
-app.listen(PORT, ()=>{
-    console.log("server is running on PORT", PORT);
-})
+app.listen(PORT, () => {
+    console.log("Server is running on PORT", PORT);
+});
